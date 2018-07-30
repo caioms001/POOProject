be5b16e7e10c311aee61c3bd2d1475c04ee4f6f0
Caio Moraes <caioms001@gmail.com>
2018-07-29 21:53:46 -0300

    Created exibirGuia

--- jogo/Tabuleiro.java
+++ jogo/Tabuleiro.java
@@ -39,7 +39,7 @@ public class Tabuleiro{
   public void exibirGuia() {
     for(int i = 1; i <= 9; i++) {
       System.out.print(" " + i + " ");
-      if(i%3 = 0 && i != 9) {
+      if(i%3 == 0 && i != 9) {
         System.out.println();
         System.out.println("-----------");
       } else
